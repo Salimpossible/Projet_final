3cc55fe22b3a30776552913aba54acdc13656527
aaugrain <60136082+aaugrain@users.noreply.github.com>
2020-03-05 16:59:57 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,6 +1,11 @@
 def terra_file_test = 
 
 node('slave') {
+        
+    def registry = "aaugrain/terra-git"
+    def registryCredential = "dockerhub"
+    def image="${registry}:version-${env.BUILD_ID}"
+    
     stage('CloningGit'){
           
         git url: 'https://github.com/aaugrain/Restful-Webservice.git',
@@ -15,21 +20,38 @@ node('slave') {
      stage('Publish test results') {
         junit 'target/surefire-reports/*.xml'
     }
+    
+    stage ('Clonegit_Dockerfile'){
+        
+        git url: 'https://github.com/salimpossible/Projet_final.git',
+            branch: 'master'
+    }
+    
+    def Image_test = stage('BuildImage'){
+        
+        docker.build("$image",  '.')
+        
+    }
+    
+    stage ('Push_DockerHub'){
+        
+        docker.withRegistry('',registryCredential){
+            Image_test.push()
 
-    stage('Binding azure credential to variable') {
-        withCredentials([file(credentialsId: '6b72eccc-25ec-4538-bb0b-3b92290b9637', variable: 'secret')])   
-
-    }
-    stage('retrieving code from git') {
-        git 'https://github.com/Salimpossible/Projet_final.git'
-    }
-    stage('Creating the test environment via a terraform container') {
-        sh ''' 
-        cd ${terra_file_test}
-        withCredentials
-        docker run -it -v $(pwd):/workspace -w /workspace hashicorp/terraform:light apply -var-file=$secret -var-file="variables/main.tfvars"
-        '''
-    }
+//    stage('Binding azure credential to variable') {
+  //      withCredentials([file(credentialsId: '6b72eccc-25ec-4538-bb0b-3b92290b9637', variable: 'secret')])   
+//
+ //   }
+//    stage('retrieving code from git') {
+ //       git 'https://github.com/Salimpossible/Projet_final.git'
+ //   }
+//    stage('Creating the test environment via a terraform container') {
+ //       sh ''' 
+  //      cd ${terra_file_test}
+   //     withCredentials
+  //      docker run -it -v $(pwd):/workspace -w /workspace hashicorp/terraform:light apply -var-file=$secret -var-file="variables/main.tfvars"
+ //       '''
+ //   }
 }
 
     // stage('Creating the test environment') {
