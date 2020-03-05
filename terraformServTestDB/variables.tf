4762eb969fb05b1cb3a2026be76f4314b4ecc2e2
Salimpossible <55975119+Salimpossible@users.noreply.github.com>
2020-03-05 16:08:06 +0100

    Update variables.tf

--- terraformServTestDB/variables.tf
+++ terraformServTestDB/variables.tf
@@ -2,7 +2,7 @@ variable "subscription_id" {}
 variable "client_id" {}
 variable  "client_secret" {}
 variable "tenant_id" {}
-# variable "version" {}
+
 variable "name" {}
 variable "location" {}
 # variable "owner" {}
