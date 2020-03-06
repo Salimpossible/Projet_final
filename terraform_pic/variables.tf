1525694c564049bb585e700bb7176cd56b2b96b8
aaugrain <60136082+aaugrain@users.noreply.github.com>
2020-03-06 01:05:28 +0100

    Update variables.tf

--- terraform_pic/variables.tf
+++ terraform_pic/variables.tf
@@ -2,7 +2,7 @@ variable subscription_id {}
 variable client_id {}
 variable client_secret {}
 variable tenant_id {}
-variable keydata {}
+variable pub_key {}
 
 variable rg_pic {}
 variable location_pic {}
