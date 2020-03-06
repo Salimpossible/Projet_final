5403cbeeeb305a66ff648f15c8c2d2eca7cc0e09
aaugrain <60136082+aaugrain@users.noreply.github.com>
2020-03-06 11:17:39 +0100

    Update main.tf

--- terraformServTestDB/main.tf
+++ terraformServTestDB/main.tf
@@ -17,14 +17,14 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 # create subnet================================================================================
-resource "azurerm_subnet" "subnet_pil1" {
-  name                 = "subnet_pil1"
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.rgpil.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = var.adress_prefix
 }
-resource "azurerm_subnet" "subnet_pil2" {
-  name                 = "subnet_pil2"
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
   resource_group_name  = azurerm_resource_group.rgpil.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = var.adress_prefix2
